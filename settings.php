841bac19e3a2e3b7936b07e391c206e6a413c041
Martin Widmann <martin.widmann@russmedia.com>
2021-02-09 20:18:37 +0100

    Start adaptation to PP Forum.

--- settings.php
+++ settings.php
@@ -1,11 +1,11 @@
 <?php
 
-    define("BOARD_URL", "https://www.maniac-forum.de/forum/pxmboard.php?mode=threadlist&brdid=");
-    define("THREAD_URL", "https://www.maniac-forum.de/forum/pxmboard.php?mode=thread&thrdid="); // must add brdid as well
-    define("MESSAGE_URL", "https://www.maniac-forum.de/forum/pxmboard.php?mode=message&msgid="); // must add brdid as well
-    define("PARANOIA_URL", "https://www.maniac-forum.de/forum/pxmboard.php?mode=messageform&msgid="); // must add brdid as well
-    
-    
-    define("POST_URL", "https://www.maniac-forum.de/forum/pxmboard.php");
-    
+    define("BOARD_URL", "http://pp-forum.p44.net/pxmboard.php?mode=threadlist&date=10000&brdid=");
+    define("THREAD_URL", "http://pp-forum.p44.net/pxmboard.php?mode=thread&thrdid="); // must add brdid as well
+    define("MESSAGE_URL", "http://pp-forum.p44.net/pxmboard.php?mode=message&msgid="); // must add brdid as well
+    define("PARANOIA_URL", "http://pp-forum.p44.net/pxmboard.php?mode=messageform&msgid="); // must add brdid as well
+
+
+    define("POST_URL", "http://pp-forum.p44.net/pxmboard.php");
+
     define("JUST_PARANOIA", FALSE);
--- weblib.php
+++ weblib.php
@@ -1,77 +1,77 @@
 <?php
-    
+
     require_once "settings.php";
-    
+
     function showForum($id) {
         $request = new HTTPRequest(BOARD_URL . $id);
-        
         $relevant_content = $request->DownloadToString();
-        $relevant_content = substr($relevant_content, 
-            stripos($relevant_content, "<p style=\"font-family:Verdana, Arial;\">") + strlen("<p style=\"font-family:Verdana, Arial;\">"));
-        $relevant_content = substr($relevant_content, 0, 
-            stripos($relevant_content, "</p>"));
-       
+        $relevant_content = substr($relevant_content, stripos($relevant_content, '<tr bgcolor="#7dacac">'));
+        // explode to the threads
+        $threads = explode('</tr>', $relevant_content);
 
-        $threads = explode( "<br>", $relevant_content);
-        // var_dump($threads);
-        //$threads = split("<br>", $relevant_content);
-        
-        $parsed_board = "";
-        for ($i = 0; $i < count($threads)-1; $i++) {
-            $is_highlight = false;
-            if (stripos($threads[$i], "fixed.gif") > 1) // see if the thread is highlighted
-                $is_highlight = true;
+        // remove the last two elements
+        array_splice($threads, -2);
 
-            $is_closed = false;
-            if (stripos($threads[$i], "closed.gif") > 1) // see if the thread is closed
-                $is_closed = true;
+        $threads = array_map(function($thread) {
+            $is_open = stripos($thread, 'img/open.png') > 0;
+            $is_fixed = stripos($thread, 'img/fixed.gif') > 0;
+            if ($is_fixed) {
+                $is_open = true;
+            }
+            if (preg_match('/<font size="2">(.*)<\/font>/m', $thread, $matches)) {
+                $title = $matches[1];
+            }
+            if (preg_match('/von (<font size="2" color="#FFC343">)?<b>(.*)<\/b>/m', $thread, $matches)) {
+                $author = $matches[2];
+                $is_mod = !empty($matches[1]);
+            }
 
-            $start_ld = stripos($threads[$i], "\"ld(");
-            if ($start_ld === false)
-                continue;
+            if (preg_match('/Antworten: (\d+)/m', $thread, $matches)) {
+                $replies = $matches[1];
+            }
+            if (preg_match('/ld\((\d+)\)/m', $thread, $matches)) {
+                $thread_id = $matches[1];
+            }
+            if (preg_match('/letzte Antwort: (\d{2}.\d{2}.\d{4} \d{2}:\d{2})/m', $thread, $matches)) {
+                $last_reply = $matches[1];
+            }
+            return [
+                'is_fixed' => $is_fixed,
+                'is_open' => $is_open,
+                'thread_id' => $thread_id,
+                'title' => $title,
+                'author' => $author,
+                'replies' => $replies,
+                'is_mod' => $is_mod,
+                'last_reply' => $last_reply,
+             ];
+        }, $threads);
 
-            $thread_id = substr($threads[$i], $start_ld + 4, stripos($threads[$i], ",", $start_ld) - ($start_ld +4));
-
-            $fontpos = stripos($threads[$i], '<font size="2">') + 15;
-            $title = substr($threads[$i], 
-                $fontpos, 
-                stripos($threads[$i], '</font', $fontpos) - $fontpos);
-
-            // var_dump( stripos($threads[$i], '<font size="2">') + 15, stripos($threads[$i], '</font') - (stripos($threads[$i], '<font size="2">') + 15) );
-
-            $author = substr($threads[$i], stripos($threads[$i], "<b>") + 3, stripos($threads[$i], "</b>") - (stripos($threads[$i], "<b>") +3));
-
-            // var_dump( $thread_id, $title, $author );
-
-            $start_antworten =  stripos($threads[$i], "Antworten: "); 
-            $num_antworten = substr($threads[$i], $start_antworten + 11, stripos($threads[$i], "<", $start_antworten) - ($start_antworten + 11));
-            if (stripos($num_antworten, ")") > 0) $num_antworten = '0';
-            $start_lastpost =  stripos($threads[$i], "Letzte: "); 
-            $lastpost = "";
-            if ($start_lastpost > 0) 
-                $lastpost = substr($threads[$i], $start_lastpost + 8, stripos($threads[$i], "<", $start_lastpost) - ($start_lastpost + 8));
-
-            $parsed_board .= "<li";
+        $counter = 0;
+        $transformed_threads = array_map(function($thread) use (&$counter, $id){
+            $thread_line .= "<li";
             $odd = "";
-            if ($i % 2 == 1) $odd = " rowodd";
-            if ($is_highlight)
-                $parsed_board .= " class=\"highlight{$odd}\"";
-            if ($is_closed)
-                $parsed_board .= " class=\"closed{$odd}\"";
-            else 
-            	$parsed_board .= " class=\"{$odd}\"";
-            	    
-            $parsed_board .= ">\n";
-            $parsed_board .= "<a href=\"thread.php?id=$id&thread=$thread_id&closed=". ($is_closed ? '1':'0') . "\">";
-            $parsed_board .= "<div class=\"title\">$title</div>\n";
-            $parsed_board .= "<div class=\"info\">Author: $author, $num_antworten Antworten, zuletzt: $lastpost</div>";
-            $parsed_board .= "</a>";
-            $parsed_board .= "</li>\n";
-        }
-        // return utf8_encode($parsed_board);
-        return ($parsed_board);
+            if ($counter % 2 == 1) $odd = " rowodd";
+            if ($thread['is_fixed'])
+                $thread_line .= " class=\"highlight{$odd}\"";
+            if (!$thread['is_open'])
+                $thread_line .= " class=\"closed{$odd}\"";
+            else
+            	$thread_line .= " class=\"{$odd}\"";
+
+            $thread_line .= ">\n";
+            $thread_line .= "<a href=\"thread.php?id=$id&thread={$thread['thread_id']}&closed=". ($thread['is_open'] ? '0':'1') . "\">";
+            $thread_line .= "<div class=\"title\">{$thread['title']}</div>\n";
+            $thread_line .= "<div class=\"info\">Author: {$thread['author']}, {$thread['replies']} Antworten, zuletzt: {$thread['last_reply']}</div>";
+            $thread_line .= "</a>";
+            $thread_line .= "</li>\n";
+            $counter ++;
+            return $thread_line;
+        }, $threads);
+
+        return utf8_encode(implode("\n", $transformed_threads));
     }
-    
+
     function showThread($board_id, $thread_id) {
         $request = new HTTPRequest(THREAD_URL . $thread_id . "&brdid=" . $board_id);
         $relevant_content = $request->DownloadToString();
@@ -82,7 +82,7 @@
         $relevant_content = preg_replace("/\<img src=\"images\/arr_off.gif\" name=\"p(\d+)\"\/\>/i", "", $relevant_content);
         $relevant_content = str_replace("<span class=\"\">", "", $relevant_content);
         $relevant_content = str_replace("</span>", "", $relevant_content);
-        
+
         $relevant_content = preg_replace("/\<font size=\"2\"\>/i", "", $relevant_content);
         $relevant_content = preg_replace("/ - \<font size=\"1\"\>/i", "<div class=\"author\">", $relevant_content);
         $relevant_content = preg_replace("/\<\/font\>/i", "", $relevant_content);
@@ -99,25 +99,25 @@
         // return utf8_encode($relevant_content);
         return ($relevant_content);
     }
-    
+
     function showMessage($board_id, $thread_id, $message_id) {
         $request = new HTTPRequest(MESSAGE_URL . $message_id . "&brdid=" . $board_id);
         $relevant_content = $request->DownloadToString();
-        
+
         $relevant_content = substr($relevant_content, stripos($relevant_content, "<td id=\"norm\"><b>") + 17);
         $title = substr($relevant_content, 0, stripos($relevant_content, "</b>"));
-        
+
         $relevant_content = substr($relevant_content, stripos($relevant_content, "&usrid=") + 7);
         $userid = substr($relevant_content, 0, stripos($relevant_content, "\""));
-        
+
         $relevant_content = substr($relevant_content, stripos($relevant_content, "hideLayer()") + 13);
         $username = substr($relevant_content, 0, stripos($relevant_content, "</a>"));
 
         $relevant_content = substr($relevant_content, stripos($relevant_content, "<font face=\"Courier New\">") + 25);
         $relevant_content = substr($relevant_content, 0, stripos($relevant_content, "</td>"));
-        
+
         $relevant_content = substr($relevant_content, 0, strripos($relevant_content, "</font>"));
-        
+
         //$relevant_content = preg_replace("#\[<a\b[^>]*>(.*?[gif|png|jpg])</a>\]#i", '<a href="\1" target="_new"><img src="\1" style="max-width:300px" border="0"/></a>', $relevant_content);
 
         $matches = array();
@@ -130,7 +130,7 @@
             }
         }
 
-        
+
         $relevant_content = "<div class=\"entry\">$relevant_content</div>";
         $relevant_content = "<div class=\"title\">$title</div>" . $relevant_content;
         if (!isset($_COOKIE['man_paranoia']) || $_COOKIE['man_paranoia'] == 1 || JUST_PARANOIA) {
@@ -139,7 +139,7 @@
             $relevant_content = "<div class=\"reply\"><a href=\"#post\" onclick=\"fillForm(this,$board_id,$message_id);document.getElementById('post').style.top=this.offsetTop - 28;\">Antworten (Inline modus)</a></div>" . $relevant_content;
         }
         $relevant_content = "<div class=\"author\">$username</div>" . $relevant_content;
-        
+
         // return utf8_encode($relevant_content);
         return ($relevant_content);
     }
@@ -192,21 +192,21 @@ class HTTPRequest
     var $_protocol;    // protocol (HTTP/HTTPS)
     var $_uri;        // request URI
     var $_port;        // port
-   
+
     // scan url
     function _scan_url()
     {
         $req = $this->_url;
-       
+
         $pos = strpos($req, '://');
         $this->_protocol = strtolower(substr($req, 0, $pos));
-       
+
         $req = substr($req, $pos+3);
         $pos = strpos($req, '/');
         if($pos === false)
             $pos = strlen($req);
         $host = substr($req, 0, $pos);
-       
+
         if(strpos($host, ':') !== false)
         {
             list($this->_host, $this->_port) = explode(':', $host);
@@ -216,29 +216,29 @@ class HTTPRequest
             $this->_host = $host;
             $this->_port = ($this->_protocol == 'https') ? 443 : 80;
         }
-       
+
         $this->_uri = substr($req, $pos);
         if($this->_uri == '')
             $this->_uri = '/';
     }
-   
+
     // constructor
     function HTTPRequest($url)
     {
         $this->_url = $url;
         $this->_scan_url();
     }
-   
+
     // download URL to string
     function DownloadToString()
     {
         $crlf = "\r\n";
-       
+
         // generate request
         $req = 'GET ' . $this->_uri . ' HTTP/1.0' . $crlf
             .    'Host: ' . $this->_host . $crlf
             .    $crlf;
-       
+
         $response ="";
         // fetch
         $this->_fp = fsockopen(($this->_protocol == 'https' ? 'ssl://' : '') . $this->_host, $this->_port);
@@ -246,21 +246,21 @@ class HTTPRequest
         while(is_resource($this->_fp) && $this->_fp && !feof($this->_fp))
             $response .= fread($this->_fp, 1024);
         fclose($this->_fp);
-       
+
         // split header and body
         $pos = strpos($response, $crlf . $crlf);
         if($pos === false)
             return($response);
         $header = substr($response, 0, $pos);
         $body = substr($response, $pos + 2 * strlen($crlf));
-       
+
         // parse headers
         $headers = array();
         $lines = explode($crlf, $header);
         foreach($lines as $line)
             if(($pos = strpos($line, ':')) !== false)
                 $headers[strtolower(trim(substr($line, 0, $pos)))] = trim(substr($line, $pos+1));
-       
+
         // redirection?
         if(isset($headers['location']))
         {
@@ -276,17 +276,17 @@ class HTTPRequest
 
 function PostRequest($url, $referer, $_data) {
     // convert variables array to string:
-    $data = array();    
+    $data = array();
     while(list($n,$v) = each($_data)){
         $data[] = urlencode($n)."=".urlencode($v);
-    }    
+    }
 
     $data = implode('&', $data);
     // format --> test1=a&test2=b etc.
 
     // parse the given URL
     $url = parse_url($url);
-    if ($url['scheme'] != 'http') { 
+    if ($url['scheme'] != 'http') {
         die('Only HTTP request are supported !');
     }
 
@@ -306,7 +306,7 @@ function PostRequest($url, $referer, $_data) {
     fputs($fp, "Connection: close\r\n\r\n");
     fputs($fp, $data);
 
-    $result = ''; 
+    $result = '';
     while(!feof($fp)) {
         // receive the results of the request
         $result .= fgets($fp, 128);
